1c0f8ce9bbf5f8e604a170f2bc8cee0944a5ebcd
RishabhKumar <rishabhkr1729@gmail.com>
2018-02-19 18:38:20 +0530

    Add data extractor for stocktwit

---
+++ data-extractor/stocktwits/stocktwits_data_extractor.py
@@ -0,0 +1,70 @@
+"""
+    EXTRACT DATA FROM STOCKTWITS API
+    WORKAROUND RATE LIMITS USING PROXY
+"""
+
+import csv
+import json
+import os
+from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
+
+FIELDS = ['symbol', 'message', 'datetime', 'user', 'message_id']
+SYMBOL = "AAPL"
+FILE_NAME = 'stocktwits_'+SYMBOL+'.csv'
+
+file = open(FILE_NAME, 'a+', newline='')
+
+# DETERMINE WHERE TO START IF RESUMING SCRIPT
+if os.stat(FILE_NAME).st_size == 0:
+    # OPEN FILE IN APPEND MODE AND WRITE HEADERS TO FILE
+    last_message_id = None
+    file = open(FILE_NAME, 'a', newline='')
+    csvfile = csv.DictWriter(file, FIELDS)
+    csvfile.writeheader()
+else:
+    # FIRST EXTRACT LAST MESSAGE ID THEN OPEN FILE IN APPEND MODE WITHOUT WRITING HEADERS
+    file = open(FILE_NAME, 'r', newline='')
+    csvfile = csv.DictReader(file)
+    data = list(csvfile)
+    data = data[-1]
+    last_message_id = data['message_id']
+    file.close()
+    file = open(FILE_NAME, 'a', newline='')
+    csvfile = csv.DictWriter(file, FIELDS)
+
+req_proxy = RequestProxy()
+
+stocktwit_url = "https://api.stocktwits.com/api/2/streams/symbol/"+SYMBOL+".json"
+if last_message_id is not None:
+    stocktwit_url += "?max="+str(last_message_id)
+
+api_hits = 0
+while True:
+    request = req_proxy.generate_proxied_request(stocktwit_url)
+    if request is not None:
+        api_hits += 1
+        response = json.loads(request.text)
+        last_message_id = response['cursor']['max']
+        # WRITE DATA TO CSV FILE
+        for message in response['messages']:
+            # PREPARE OBJECT TO WRITE IN CSV FILE
+            obj = {}
+            obj['symbol'] = SYMBOL
+            obj['message'] = message['body']
+            obj['datetime'] = message['created_at']
+            obj['user'] = message['user']['id']
+            obj['message_id'] = message['id']
+
+            csvfile.writerow(obj)
+
+
+        print("API HITS TILL NOW = {}".format(api_hits))
+
+        # NO MORE MESSAGES
+        if not response['messages']:
+            break
+
+    # ADD MAX ARGUMENT TO GET OLDER MESSAGES
+    stocktwit_url = "https://api.stocktwits.com/api/2/streams/symbol/"+SYMBOL+".json"+"?max="+str(last_message_id)
+
+file.close()
