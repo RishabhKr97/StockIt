0d88b04517a0c9b45da08aa1054692cfd0570767
RishabhKumar <rishabhkr1729@gmail.com>
2018-03-23 12:30:46 +0530

    Implement data preprocessing

--- load_data.py
+++ load_data.py
@@ -2,8 +2,10 @@
     handle preprocessing and loading of data.
 """
 
+import html
 import os.path
 import pandas as pd
+import re
 
 class LoadData:
 
@@ -14,11 +16,16 @@ class LoadData:
             preprocesses in following ways:
             1) extract message and datetime columns.
             2) sort according to datetime in descending order (newest first)
-            3) decode HTML entities
+            3) remove links, @ references, extra whitespaces
+            4) decode html entities
         """
 
         dataFrame = pd.read_csv('data-extractor/stocktwits_'+symbol+'.csv', usecols=['datetime', 'message'], parse_dates=['datetime'], infer_datetime_format=True)
         dataFrame.sort_values(by='datetime', ascending=False)
+        dataFrame['message'] = dataFrame['message'].apply(lambda x: re.sub('(www\.|https?://).*?(\s|$)|@.*?(\s|$)|\s+', ' ', x))
+        dataFrame['message'] = dataFrame['message'].apply(lambda x: re.sub('^\s*|\s*$', '', x))
+        dataFrame['message'] = dataFrame['message'].apply(lambda x: html.unescape(x))
+
         dataFrame.to_csv('data-extractor/stocktwits_'+symbol+'_preprocessed.csv', index=False)
 
     @classmethod
