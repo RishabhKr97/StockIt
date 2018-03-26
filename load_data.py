1ea6fcaa965aed81fae438725a50adf631f30985
RishabhKumar <rishabhkr1729@gmail.com>
2018-03-26 11:19:37 +0530

    Modify data preprocessor to preprocess labelled data as well

--- load_data.py
+++ load_data.py
@@ -10,23 +10,27 @@ import re
 class LoadData:
 
     @classmethod
-    def preprocess_stocktwits_data(cls, symbol):
+    def preprocess_stocktwits_data(cls, file_location, columns=['datetime', 'message']):
         """
-            preprocess the data and saves it as a csv file (stocktwits_'symbol'_preprocessed.csv)
-            preprocesses in following ways:
+            preprocess the data in file location and saves it as a csv file (appending
+            '_preprocessed' before '.csv). The preprocessing us in following ways:
             1) extract message and datetime columns.
             2) sort according to datetime in descending order (newest first)
             3) remove links, @ references, extra whitespaces
             4) decode html entities
         """
 
-        dataFrame = pd.read_csv('data-extractor/stocktwits_'+symbol+'.csv', usecols=['datetime', 'message'], parse_dates=['datetime'], infer_datetime_format=True)
-        dataFrame.sort_values(by='datetime', ascending=False)
+        if 'datetime' in columns:
+            dataFrame = pd.read_csv(file_location, usecols=columns, parse_dates=['datetime'], infer_datetime_format=True)
+            dataFrame.sort_values(by='datetime', ascending=False)
+        else:
+            dataFrame = pd.read_csv(file_location, usecols=columns)
+
         dataFrame['message'] = dataFrame['message'].apply(lambda x: re.sub('(www\.|https?://).*?(\s|$)|@.*?(\s|$)|\s+', ' ', x))
         dataFrame['message'] = dataFrame['message'].apply(lambda x: re.sub('^\s*|\s*$', '', x))
         dataFrame['message'] = dataFrame['message'].apply(lambda x: html.unescape(x))
 
-        dataFrame.to_csv('data-extractor/stocktwits_'+symbol+'_preprocessed.csv', index=False)
+        dataFrame.to_csv(file_location[:-4]+'_preprocessed.csv', index=False)
 
     @classmethod
     def get_stocktwits_data(cls, symbol):
@@ -37,7 +41,21 @@ class LoadData:
 
         file_location = 'data-extractor/stocktwits_'+symbol+'_preprocessed.csv'
         if os.path.isfile(file_location) is False:
-            LoadData.preprocess_stocktwits_data(symbol)
+            LoadData.preprocess_stocktwits_data('data-extractor/stocktwits_'+symbol+'.csv')
+
+        dataFrame = pd.read_csv(file_location)
+        return dataFrame
+
+    @classmethod
+    def get_labelled_data(cls):
+        """
+            get_labelled_data loads the preprocessed labelled data of stocktwits from data-extractor
+            and returns a pandas dataframe with columns [sentiment(object), message(object)].
+        """
+
+        file_location = 'data-extractor/labelled_data_preprocessed.csv'
+        if os.path.isfile(file_location) is False:
+            LoadData.preprocess_stocktwits_data('data-extractor/labelled_data.csv', columns=['sentiment', 'message'])
 
         dataFrame = pd.read_csv(file_location)
         return dataFrame
