d47fa2417509077051395f67355aa8a45210fee4
JatinThareja <anexus7@gmail.com>
2018-03-29 14:22:44 +0530

    Added RNN to classify tweets

---
+++ tweet_classification_using_rnn.py
@@ -0,0 +1,229 @@
+import nltk
+import string
+import re
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+data = pd.read_csv('data-extractor/sentiment_db.csv')
+
+del data['message_id']
+del data['symbol']
+custom_stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll",
+                     "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's",
+                     'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs',
+                     'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am',
+                     'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
+                     'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while',
+                     'of', 'at', 'by', 'for', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all',
+                     'any', 'both', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 's', 't', 'can', 'will', 'just',
+                     'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren',
+                     "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn',
+                     "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't",
+                     'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't",
+                     'won', "won't", 'wouldn', "wouldn't", "to"]
+
+stop_words = set(custom_stop_words)
+
+
+def tokenizer(s):
+    s = re.sub(r'http\S+', '', s)
+    s = re.sub(r'\$(\w+)', '', s)
+    translate_table = dict((ord(char), None) for char in string.punctuation)
+    s = s.translate(translate_table)
+    tokens = nltk.word_tokenize(s)
+    filtered_tokens = []
+    for word in tokens:
+        if word.lower() not in stop_words:
+            filtered_tokens.append(word.lower())
+    return filtered_tokens
+
+
+data['message'] = data['message'].apply(tokenizer)
+data['message_length'] = data['message'].apply(len)
+data = data[(data['message_length'] > 0)]
+
+
+def func(x):
+    if x == 'Bullish':
+        return 1
+    return 0
+
+
+data.sentiment = data.sentiment.apply(func)
+words = []
+
+for message in data.message:
+    for word in message:
+        words.append(word)
+
+from collections import Counter
+
+counts = Counter(words)
+vocab = sorted(counts, key=counts.get, reverse=True)
+vocab_to_int = {word: ii for ii, word in enumerate(vocab, 1)}
+max_len = data.message_length.max()
+
+
+def fun(x):
+    vec = []
+    zeroes = max_len - len(x)
+    for i in range(zeroes):
+        vec.append(0)
+    for word in x:
+        vec.append(vocab_to_int[word])
+    return np.array(vec)
+
+
+data['encoded_message'] = data['message'].apply(fun)
+
+del data['message']
+del data['message_length']
+
+features = []
+for vec in data.encoded_message:
+    features.append(vec.tolist())
+features = np.array(features)
+np.random.shuffle(features)
+features = features[:80000]
+split_frac = 0.7
+
+split_index = int(split_frac * len(features))
+
+train_x, val_x = features[:split_index], features[split_index:]
+train_y, val_y = data.sentiment[:split_index], data.sentiment[split_index:len(features)]
+
+split_frac = 0.3
+split_index = int(split_frac * len(val_x))
+
+val_x, test_x = val_x[:split_index], val_x[split_index:]
+val_y, test_y = val_y[:split_index], val_y[split_index:]
+
+print("\t\t\tFeature Shapes:")
+print("Train set: \t\t{}".format(train_x.shape),
+      "\nValidation set: \t{}".format(val_x.shape),
+      "\nTest set: \t\t{}".format(test_x.shape))
+print("label set: \t\t{}".format(train_y.shape),
+      "\nValidation label set: \t{}".format(val_y.shape),
+      "\nTest label set: \t\t{}".format(test_y.shape))
+
+lstm_size = 256
+lstm_layers = 2
+batch_size = 1000
+learning_rate = 0.01
+
+n_words = len(vocab_to_int) + 1  # Add 1 for 0 added to vocab
+
+# Create the graph object
+tf.reset_default_graph()
+with tf.name_scope('inputs'):
+    inputs_ = tf.placeholder(tf.int32, [None, None], name="inputs")
+    labels_ = tf.placeholder(tf.int32, [None, None], name="labels")
+    keep_prob = tf.placeholder(tf.float32, name="keep_prob")
+
+embed_size = 300
+
+with tf.name_scope("Embeddings"):
+    embedding = tf.Variable(tf.random_uniform((n_words, embed_size), -1, 1))
+    embed = tf.nn.embedding_lookup(embedding, inputs_)
+
+
+def lstm_cell():
+    # Your basic LSTM cell
+    lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size, reuse=tf.get_variable_scope().reuse)
+    # Add dropout to the cell
+    return tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=keep_prob)
+
+
+with tf.name_scope("RNN_layers"):
+    # Stack up multiple LSTM layers, for deep learning
+    cell = tf.contrib.rnn.MultiRNNCell([lstm_cell() for _ in range(lstm_layers)])
+
+    # Getting an initial state of all zeros
+    initial_state = cell.zero_state(batch_size, tf.float32)
+
+with tf.name_scope("RNN_forward"):
+    outputs, final_state = tf.nn.dynamic_rnn(cell, embed, initial_state=initial_state)
+
+with tf.name_scope('predictions'):
+    predictions = tf.contrib.layers.fully_connected(outputs[:, -1], 1, activation_fn=tf.sigmoid)
+    tf.summary.histogram('predictions', predictions)
+with tf.name_scope('cost'):
+    cost = tf.losses.mean_squared_error(labels_, predictions)
+    tf.summary.scalar('cost', cost)
+
+with tf.name_scope('train'):
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+
+merged = tf.summary.merge_all()
+
+with tf.name_scope('validation'):
+    correct_pred = tf.equal(tf.cast(tf.round(predictions), tf.int32), labels_)
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+
+def get_batches(x, y, batch_size=100):
+    n_batches = len(x) // batch_size
+    x, y = x[:n_batches * batch_size], y[:n_batches * batch_size]
+    for ii in range(0, len(x), batch_size):
+        yield x[ii:ii + batch_size], y[ii:ii + batch_size]
+
+
+epochs = 10
+
+# with graph.as_default():
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    train_writer = tf.summary.FileWriter('./logs/tb/train', sess.graph)
+    test_writer = tf.summary.FileWriter('./logs/tb/test', sess.graph)
+    iteration = 1
+    for e in range(epochs):
+        state = sess.run(initial_state)
+
+        for ii, (x, y) in enumerate(get_batches(train_x, train_y, batch_size), 1):
+            feed = {inputs_: x,
+                    labels_: y[:, None],
+                    keep_prob: 0.5,
+                    initial_state: state}
+            summary, loss, state, _ = sess.run([merged, cost, final_state, optimizer], feed_dict=feed)
+            #             loss, state, _ = sess.run([cost, final_state, optimizer], feed_dict=feed)
+
+            train_writer.add_summary(summary, iteration)
+
+            if iteration % 5 == 0:
+                print("Epoch: {}/{}".format(e, epochs),
+                      "Iteration: {}".format(iteration),
+                      "Train loss: {:.3f}".format(loss))
+
+            if iteration % 25 == 0:
+                val_acc = []
+                val_state = sess.run(cell.zero_state(batch_size, tf.float32))
+                for x, y in get_batches(val_x, val_y, batch_size):
+                    feed = {inputs_: x,
+                            labels_: y[:, None],
+                            keep_prob: 1,
+                            initial_state: val_state}
+                    #                     batch_acc, val_state = sess.run([accuracy, final_state], feed_dict=feed)
+                    summary, batch_acc, val_state = sess.run([merged, accuracy, final_state], feed_dict=feed)
+                    val_acc.append(batch_acc)
+                print("Val acc: {:.3f}".format(np.mean(val_acc)))
+            iteration += 1
+            test_writer.add_summary(summary, iteration)
+            saver.save(sess, "checkpoints/sentiment_jatin.ckpt")
+    saver.save(sess, "checkpoints/sentiment_jatin.ckpt")
+
+test_acc = []
+
+with tf.Session() as sess:
+    saver.restore(sess, "checkpoints/sentiment_jatin.ckpt")
+    test_state = sess.run(cell.zero_state(batch_size, tf.float32))
+    for ii, (x, y) in enumerate(get_batches(test_x, test_y, batch_size), 1):
+        feed = {inputs_: x,
+                labels_: y[:, None],
+                keep_prob: 1,
+                initial_state: test_state}
+        batch_acc, test_state = sess.run([accuracy, final_state], feed_dict=feed)
+        test_acc.append(batch_acc)
+    print("Test accuracy: {:.3f}".format(np.mean(test_acc)))
