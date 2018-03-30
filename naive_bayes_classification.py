85777485a9dbfcf9acab8bb909d4717dbacc066a
RishabhKumar <rishabhkr1729@gmail.com>
2018-03-30 15:42:30 +0530

    Change pipeline according to grid search optimization

--- naive_bayes_classification.py
+++ naive_bayes_classification.py
@@ -9,6 +9,7 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
+from sklearn.model_selection import GridSearchCV
 
 class NaiveBayes:
 
@@ -17,8 +18,17 @@ class NaiveBayes:
         dataFrameTraining = load_data.LoadData.get_labelled_data(type='training')
 
         # make a pipeline for transforms
-        tweet_classifier = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB())])
+        # tweet_classifier = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB())])
+        tweet_classifier = Pipeline([('vect', CountVectorizer(ngram_range=(1,2))), ('clf', MultinomialNB(alpha=0.01))])
         tweet_classifier.fit(dataFrameTraining['message'].values, dataFrameTraining['sentiment'].values)
+
+        # grid search for best params
+        # parameters ={'vect__ngram_range': [(1, 1), (1, 2)], 'tfidf__use_idf': (True, False),'clf__alpha': (1e-2, 1e-3)}
+        # gridsearch = GridSearchCV(tweet_classifier, parameters, n_jobs=-1)
+        # gridsearch = gridsearch.fit(dataFrameTraining['message'].values, dataFrameTraining['sentiment'].values)
+        # print(gridsearch.best_score_)
+        # print(gridsearch.best_params_)
+        # print(gridsearch.cv_results_)
 
         # save the trained classifier
         file_location = 'naive_bayes_classifier.pkl'
